cdfa8291004cb317cd125a6612832f92f460abe6
lzenner <lzenner@live.com>
2020-09-27 09:47:24 -1000

    Renumbered Info portion strings to start at 2000
    This is just so I can more easily determine in which file the strings originated

--- framedagain/dialogues/framed_info.d
+++ framedagain/dialogues/framed_info.d
@@ -40,91 +40,91 @@ END
 
 CHAIN  
 	IF ~~ THEN IDJINNI NO_JOURNAL_2
-		@3002 /* ~For the murder of Skie Silvershield.  He killed her and trapped her soul inside a soultaker dagger.~ */
-		== %IMOEN_JOINED% IF ~IsValidForPartyDialogue("IMOEN2")~ @3003 /* ~Oh how awful!~ */
+		@2002 /* ~For the murder of Skie Silvershield.  He killed her and trapped her soul inside a soultaker dagger.~ */
+		== %IMOEN_JOINED% IF ~IsValidForPartyDialogue("IMOEN2")~ @2003 /* ~Oh how awful!~ */
 	END
-	++ @3004 /*~But what makes them think I did it.~ */ + NO_JOURNAL_3
+	++ @2004 /*~But what makes them think I did it.~ */ + NO_JOURNAL_3
 
 CHAIN  
 	IF ~~ THEN IDJINNI JOURNAL_2
-		@3002 /* ~For the murder of Skie Silvershield.  He killed her and trapped her soul inside a soultaker dagger.~ */
-		== %IMOEN_JOINED% IF ~IsValidForPartyDialogue("IMOEN2")~ @3003 /* ~Oh how awful!~ */
+		@2002 /* ~For the murder of Skie Silvershield.  He killed her and trapped her soul inside a soultaker dagger.~ */
+		== %IMOEN_JOINED% IF ~IsValidForPartyDialogue("IMOEN2")~ @2003 /* ~Oh how awful!~ */
 	END
-	++ @3004 /*~But what makes them think I did it.~ */ + JOURNAL_3
+	++ @2004 /*~But what makes them think I did it.~ */ + JOURNAL_3
 
 
 APPEND IDJINNI
 	IF ~~ BEGIN NO_QUEST_FULL_INFO_NO_JOURNAL_1
-		SAY @3000 /* ~It is unfortunate that I no longer am the keeper of the dagger the master used to frame you, or I would be able to offer it to you to sweeten the deal.~ */
-		++ @3001 /* ~Frame me?  For what?~ */ + NO_JOURNAL_2
+		SAY @2000 /* ~It is unfortunate that I no longer am the keeper of the dagger the master used to frame you, or I would be able to offer it to you to sweeten the deal.~ */
+		++ @2001 /* ~Frame me?  For what?~ */ + NO_JOURNAL_2
 	END
 
 	IF ~~ BEGIN NO_QUEST_FULL_INFO_JOURNAL_1
-		SAY @3000 /* ~It is unfortunate that I no longer am the keeper of the dagger the master used to frame you, or I would be able to offer it to you to sweeten the deal.~ */
-		++ @3001 /* ~Frame me?  For what?~ */ + JOURNAL_2
+		SAY @2000 /* ~It is unfortunate that I no longer am the keeper of the dagger the master used to frame you, or I would be able to offer it to you to sweeten the deal.~ */
+		++ @2001 /* ~Frame me?  For what?~ */ + JOURNAL_2
 	END
 
 	IF ~~ BEGIN LAVA_QUEST_FULL_INFO_NO_JOURNAL_1
-		SAY @3007 /* ~To sweeten the deal, I will give you the dagger the master used to frame you.  I will have no use for it.~ */
-		++ @3001 /* ~Frame me?  For what?~ */ + NO_JOURNAL_2
+		SAY @2007 /* ~To sweeten the deal, I will give you the dagger the master used to frame you.  I will have no use for it.~ */
+		++ @2001 /* ~Frame me?  For what?~ */ + NO_JOURNAL_2
 	END
 
 	IF ~~ BEGIN LAVA_QUEST_FULL_INFO_JOURNAL_1
-		SAY @3007 /* ~To sweeten the deal, I will give you the dagger the master used to frame you.  I will have no use for it.~ */
-		++ @3001 /* ~Frame me?  For what?~ */ + JOURNAL_2
+		SAY @2007 /* ~To sweeten the deal, I will give you the dagger the master used to frame you.  I will have no use for it.~ */
+		++ @2001 /* ~Frame me?  For what?~ */ + JOURNAL_2
 	END
 
 	IF ~~ BEGIN NO_QUEST_NO_INFO_NO_JOURNAL_1
-		SAY @3000 /* ~It is unfortunate that I no longer am the keeper of the dagger the master used to frame you, or I would be able to offer it to you to sweeten the deal.~ */
-		= @3006 /* ~But unfortunately, I suspect the dagger has been destroyed since he no longer had any use for it.~ */
+		SAY @2000 /* ~It is unfortunate that I no longer am the keeper of the dagger the master used to frame you, or I would be able to offer it to you to sweeten the deal.~ */
+		= @2006 /* ~But unfortunately, I suspect the dagger has been destroyed since he no longer had any use for it.~ */
 		IF ~~ THEN EXIT
 	END
 
 	IF ~~ BEGIN NO_QUEST_NO_INFO_JOURNAL_1
-		SAY @3000 /* ~It is unfortunate that I no longer am the keeper of the dagger the master used to frame you, or I would be able to offer it to you to sweeten the deal.~ */
-		= @3006 /* ~But unfortunately, I suspect the dagger has been destroyed since he no longer had any use for it.~ */
+		SAY @2000 /* ~It is unfortunate that I no longer am the keeper of the dagger the master used to frame you, or I would be able to offer it to you to sweeten the deal.~ */
+		= @2006 /* ~But unfortunately, I suspect the dagger has been destroyed since he no longer had any use for it.~ */
   		IF ~~ THEN DO ~SetGlobal("HelpDjinni","GLOBAL",1)~ UNSOLVED_JOURNAL #34099 EXIT
 	END
 
 	IF ~~ BEGIN LAVA_QUEST_NO_INFO_NO_JOURNAL_1
-		SAY @3007 /* ~To sweeten the deal, I will give you the dagger the master used to frame you.  I will have no use for it.~ */
+		SAY @2007 /* ~To sweeten the deal, I will give you the dagger the master used to frame you.  I will have no use for it.~ */
 		IF ~~ THEN EXIT
 	END
 
 	IF ~~ BEGIN LAVA_QUEST_NO_INFO_JOURNAL_1
-		SAY @3007 /* ~To sweeten the deal, I will give you the dagger the master used to frame you.  I will have no use for it.~ */
+		SAY @2007 /* ~To sweeten the deal, I will give you the dagger the master used to frame you.  I will have no use for it.~ */
   		IF ~~ THEN DO ~SetGlobal("HelpDjinni","GLOBAL",1)~ UNSOLVED_JOURNAL #34099 EXIT
 	END
 
 	IF ~~ BEGIN NO_JOURNAL_3
-		SAY @3005 /* ~How he managed to cause others to believe you were the one who killed her, I do not know.~ */
+		SAY @2005 /* ~How he managed to cause others to believe you were the one who killed her, I do not know.~ */
 		IF ~Global("#L_DestroyedDagger","GLOBAL",0)~ GOTO NO_QUEST_NO_JOURNAL_4
 		IF ~Global("#L_DestroyedDagger","GLOBAL",1)~ GOTO LAVA_QUEST_NO_JOURNAL_4
 	END
 
 	IF ~~ BEGIN JOURNAL_3
-		SAY @3005 /* ~How he managed to cause others to believe you were the one who killed her, I do not know.~ */
+		SAY @2005 /* ~How he managed to cause others to believe you were the one who killed her, I do not know.~ */
 		IF ~Global("#L_DestroyedDagger","GLOBAL",0)~ GOTO NO_QUEST_JOURNAL_4
 		IF ~Global("#L_DestroyedDagger","GLOBAL",1)~ GOTO LAVA_QUEST_JOURNAL_4
 	END
 
 	IF ~~ BEGIN NO_QUEST_NO_JOURNAL_4
-		SAY @3006 /* ~But unfortunately, I suspect the dagger has been destroyed since he no longer had any use for it.~ */
+		SAY @2006 /* ~But unfortunately, I suspect the dagger has been destroyed since he no longer had any use for it.~ */
 		IF ~~ THEN EXIT
 	END
 
 	IF ~~ BEGIN LAVA_QUEST_NO_JOURNAL_4
-		SAY @3008 /* ~I am protecting the dagger for the master.  If you free me, I will you give it and your weapon.~ */
+		SAY @2008 /* ~I am protecting the dagger for the master.  If you free me, I will you give it and your weapon.~ */
 		IF ~~ THEN EXIT
 	END
 
 	IF ~~ BEGIN NO_QUEST_JOURNAL_4
-		SAY @3006 /* ~But unfortunately, I suspect the dagger has been destroyed since he no longer had any use for it.~ */
+		SAY @2006 /* ~But unfortunately, I suspect the dagger has been destroyed since he no longer had any use for it.~ */
   		IF ~~ THEN DO ~SetGlobal("HelpDjinni","GLOBAL",1)~ UNSOLVED_JOURNAL #34099 EXIT
 	END
 
 	IF ~~ BEGIN LAVA_QUEST_JOURNAL_4
-		SAY @3008 /* ~I am protecting the dagger for the master.  If you free me, I will you give it and your weapon.~ */
+		SAY @2008 /* ~I am protecting the dagger for the master.  If you free me, I will you give it and your weapon.~ */
   		IF ~~ THEN DO ~SetGlobal("HelpDjinni","GLOBAL",1)~ UNSOLVED_JOURNAL #34099 EXIT
 	END
 
